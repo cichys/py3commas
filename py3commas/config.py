2e65fe8a7dfcb197535c5ce27c02c2607c26d9b7
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2019-11-03 16:59:55 +0700

    Request

--- py3commas/config.py
+++ py3commas/config.py
@@ -1,2 +1,10 @@
 API_URL = 'https://api.3commas.io'
-API_URL_PATH = '/public/api/ver1/'
+API_VERSION = '/public/api/ver1/'
+
+APIS = {
+    'smart_trades': {
+        '': ('GET', ''),
+        'step_panic_sell': ('POST', '{id}/step_panic_sell'),
+        'update': ('POST', '{id}/update')
+    }
+}
