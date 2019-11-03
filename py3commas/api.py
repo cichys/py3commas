133e4051953c7e56a543bba3d725c514b8599346
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2019-11-03 16:24:09 +0700

    Fix signature generation

--- py3commas/api.py
+++ py3commas/api.py
@@ -1,6 +1,5 @@
 import hashlib
 import hmac
-import binascii
 import requests
 from .config import API_URL, API_URL_PATH
 
@@ -15,21 +14,19 @@ class Api:
 
     def generate_signature(self, path: str, data: str) -> str:
         total_params = self.url_path + path + data
-        byte_key = binascii.unhexlify(self.secret)
-        message = total_params.encode()
+        byte_key = str.encode(self.secret)
+        message = str.encode(total_params)
         signature = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
-        print("signature = {0}".format(signature))
         return signature
 
     def make_request(self, path: str, params: any):
         signature = self.generate_signature(path, params)
 
         response = requests.get(
-            self.url + self.url_path + path,
+            self.url + self.url_path + path + '?' + params,
             headers={
                 'APIKEY': self.key,
                 'Signature': signature
             }
         )
         return response
-
